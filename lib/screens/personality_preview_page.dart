1e61227e5a20f5c8678ccdcc5b574ce547d5a8c7
Daniyal Ahmad Rizwan <68680852+itsDaniyaL@users.noreply.github.com>
2024-10-02 08:42:56 +0500

    added personality preview page

--- lib/screens/personality_preview_page.dart
+++ lib/screens/personality_preview_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:personality_checker/shared/filled_button.dart';
+import 'package:personality_checker/shared/picked_options_list.dart';
 
 class PersonalityPreviewPage extends StatefulWidget {
   const PersonalityPreviewPage({super.key});
@@ -10,8 +13,102 @@ class PersonalityPreviewPage extends StatefulWidget {
 class _PersonalityPreviewPageState extends State<PersonalityPreviewPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Checking"),
+    return Scaffold(
+      floatingActionButton: CustomFilledButton(
+        onPressed: () {},
+        textColor: Colors.white,
+        buttonColor: const Color(0xFF1E1515),
+        child: const Text("Recheck",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            )),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: SvgPicture.asset(
+                  'assets/images/illustration_1.svg',
+                  width: 30,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Text(
+                      "All done",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "You are more of an",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    const Text("introvert",
+                        style: TextStyle(
+                            color: Color(0xFFFD9191),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Text(
+                        "You feel that living alone is to live happily, and you prefer hiding in a crowd rather than standing out in one. You are perpetually tormented by the idea of doing things wrong, not understanding or not being alert enough or intelligent enough to do what others expect of you. You lack in self-confidence and you seem to believe that others are better than you. While in a conversation, for example, you would be more likely to go along with the other’s points of view as you don’t fully respect your own opinions.",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset(
+                        'assets/images/introvert_illustration.svg',
+                        width: 125,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 30.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Your choices",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                    const PickedOptionsList(),
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: SvgPicture.asset(
+                  'assets/images/illustration_2.svg',
+                  width: 60,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/shared/picked_options_list.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:personality_checker/state/app_state.dart';
+import 'package:provider/provider.dart';
+
+class PickedOptionsList extends StatelessWidget {
+  const PickedOptionsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppState>(builder: (context, state, child) {
+      final questions = state.questions;
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            children: List.generate(questions.length, (index) {
+          final selectedOption = questions[index].selectedOption != null
+              ? questions[index]
+                  .options
+                  .keys
+                  .elementAt(questions[index].selectedOption!)
+              : null;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.height * 0.35,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 180),
+                child: IntrinsicHeight(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                                questions[index]
+                                    .title
+                                    .replaceAll("\n\nYou:", ""),
+                                style: const TextStyle(fontSize: 12)),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: const Color(0xFF1E1515)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  selectedOption ?? "",
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+              ),
+            ),
+          );
+        })),
+      );
+    });
+  }
+}
